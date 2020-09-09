43f01d49f7cec9f04376470cc51aa0b9602d2b13
Marcel <68339367+Draknador@users.noreply.github.com>
2020-09-09 20:28:11 +0200

    Update cl_wave.lua

--- wave/lua/autorun/client/cl_wave.lua
+++ wave/lua/autorun/client/cl_wave.lua
@@ -1,46 +1,12 @@
-sound.Add({
-    name = "pumped",
-    channel = CHAN_STATIC,
-    volume = 35,
-    level = 100,
-    pitch = {95, 110},
-    sound = "wave/pumped.wav"
-})
-/*
-
-
-function icherstelleeinenlog()
+hook.Add("Initialize", "Dateiencreate", function()
     if not file.Exists("meinlog", "DATA") then
         file.CreateDir("meinlog")
         file.Write("meinlog/draklog.txt", "//////////// IM FOLGENDEN HÄNGEN WIR DEN CONTENT AN ////////////")
-        file.Write("meinlog/befehle.txt")
-    end
-end
-
-hook.Add("Initialize", "macheinedatei", icherstelleeinenlog)
-*/
-
-hook.Add("SpawnMenuOpen", "spawnmenuoefnnen", function()
-    if not spawnmenuoeffnenlassen[LocalPlayer():SteamID()] then
-        return false
-    else
-        return true
     end
 end)
 
-net.Receive("pumped", function(len)
-    local ply = LocalPlayer()
-    local spielpumpedup = net.ReadBool()
-
-    if (spielpumpedup) then
-        ply:EmitSound("pumped")
-
-        if not timer.Exists("PumpedSound") then
-            timer.Create("PumpedSound", 51, 0, function()
-                ply:EmitSound("pumped")
-            end)
-        end
-    end
+hook.Add("SpawnMenuOpen", "spawnmenuoefnnen", function()
+    if not spawnmenuoeffnenlassen[LocalPlayer():SteamID()] then return false end
 end)
 
 net.Receive("playerspawns", function(len)
@@ -49,14 +15,19 @@ net.Receive("playerspawns", function(len)
     ply:PrintMessage(HUD_PRINTTALK, "Der Spieler " .. ReceivedString .. " ist aufgetaucht!")
 end)
 
-concommand.Add("stoploop", function()
-    timer.Stop("PumpedSound")
-end)
-
-function GM:PlayerSetModel(ply)
-    if not playermodelsetzten[LocalPlayer():SteamID()] then
-        return false
-    else
-        ply:SetModel("models/player/shi/felix_argyle.mdl")
+function Logsdatei()
+    if not file.Exists("derlog", "DATA") then
+        file.CreateDir("derlog")
+        file.Write("derlog/draknador.txt", "Hier d'runter entsteht Magie!")
     end
 end
+
+hook.Add("Initialize", "ErstelleDateien", Logsdatei)
+net.Receive("playerspawns")
+
+net.Receive("playerspawns", function()
+    local Timestamp = os.time()
+    local TimeString = os.date("%d.%m.%Y - %H:%M:%S", Timestamp)
+    local spieler = net.ReadString()
+    file.Append("derlog/draknador.txt", "\n[" .. TimeString .. "] " .. spieler .. " hat sich auf den Server verbunden.")
+end)
